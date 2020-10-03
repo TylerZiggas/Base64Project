d8f225746f02bd143dbcc9891b16736d8913db00
twzmmp <twzmmp@umsystem.edu>
2020-10-03 18:37:22 -0500

    Ready for review

---
+++ source/production/java/edu/umsl/base64conversion.java
@@ -0,0 +1,24 @@
+package edu.umsl;
+
+import org.apache.commons.codec.binary.Base64;
+import java.util.Scanner;
+
+
+public class base64conversion {
+    public static void main(String args[]) {
+    String encodedString = "SSBsb3ZlIG15IENNUCBTQ0kgMjI2MSBjbGFzcyBzbyBtdWNoLCBJIHdpc2ggSSBjb3VsZCBiZSBqdXN0IGFzIGNvb2wgYXMgbXkgcHJvZmVzc29y"; // Needed string
+    byte[] decodedBytes = Base64.decodeBase64(encodedString); // Decode using the apache base 64 conversion
+    String decodedString = new String(decodedBytes); // Turn into a string
+    System.out.println("The code for the project is: " + encodedString);
+    System.out.println("The code translates to:  " + decodedString + "\n");
+
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a string to encode: "); // User input to encode into Base64
+    String stringToEncode = input.nextLine();
+    byte[] encodedByte = stringToEncode.getBytes();
+
+    String finalString = "";
+    finalString = Base64.encodeBase64String(encodedByte); // Encode the string
+    System.out.println("The encoded string is: " + finalString);
+    }
+}
